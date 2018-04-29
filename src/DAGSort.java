d76d9fe6c97e501ec8484b707cca81a34de76581
Radoslav Enev <rado_bg98@abv.bg>
2018-04-29 20:46:58 +0100

    New code
    Blyat2

--- src/DAGSort.java
+++ src/DAGSort.java
@@ -4,6 +4,27 @@ import java.util.List;
 
 public class DAGSort {
 
+    /**
+     * This method takes a directed acyclic graph as input and returns a
+     * topological sort of the nodes. Nodes are assumed to be laballed from 0 to
+     * (number of nodes) - 1.
+     *
+     *
+     * @param edges
+     *            An array representing the edges of the graph. Each row
+     *            contains the immediate out-neighbours of a particular node.
+     *            E.g., edges[0] = {1,2,5} means there are edges from node 0 to
+     *            nodes 1, 2 and 5.
+     * @return A valid topological order of the nodes, i.e., every node must
+     *         appear before its out-neighbours in this order.
+     * @throws CycleDetectedException
+     *             If the input graph has cycles.
+     * @throws NullPointerException
+     *             If null is provided as a parameter.
+     * @throws InvalidNodeException
+     *             If the edges parameter contains edges that are not labelled
+     *             from 0 to (edges.length - 1)
+     */
     public static int[] sortDAG(int[][] edges)
             throws CycleDetectedException, InvalidNodeException {
         if (edges == null) {
@@ -17,7 +38,7 @@ public class DAGSort {
                 }
             }
         }
-        List reverseSorted = new ArrayList(edges.length);
+        List<Integer> reverseSorted = new ArrayList<Integer>(edges.length);
         boolean[] visiting = new boolean[edges.length];
         boolean[] visited = new boolean[edges.length];
         Arrays.fill(visiting, false);
@@ -34,8 +55,24 @@ public class DAGSort {
         return sorted;
     }
 
+    /**
+     * Carry out a (recursive) depth-first search from this node.
+     *
+     * @param i
+     *            The index of the starting node.
+     * @param edges
+     *            The edges of the graph.
+     * @param visiting
+     *            The nodes that are currently being visited as part of the DFS.
+     * @param visited
+     *            The nodes that have already been visited.
+     * @param reverseSorted
+     *            The final topological order (in reverse).
+     * @throws CycleDetectedException
+     *             If the DFS encounters a cycle.
+     */
     private static void visit(int i, int[][] edges, boolean[] visiting,
-                              boolean[] visited, List reverseSorted)
+                              boolean[] visited, List<Integer> reverseSorted)
             throws CycleDetectedException {
         if (visiting[i]) {
             throw new CycleDetectedException();
@@ -51,4 +88,20 @@ public class DAGSort {
         }
     }
 
+}
+
+class CycleDetectedException extends Exception {
+    public CycleDetectedException() {
+        super();
+    }
+}
+
+class InvalidNodeException extends Exception {
+    public InvalidNodeException() {
+        super();
+    }
+
+    public InvalidNodeException(String s) {
+        super(s);
+    }
 }
